5966665ae86f6766af74433a7d441e3f1a70e648
Nitive <samoilowmaxim@gmail.com>
2015-04-20 10:51:27 +0600

    Delete publish.sh

--- publish.sh
+++
@@ -1,8 +0,0 @@
-git checkout master
-git branch -D public
-git checkout -b public
-gulp product
-git add -f public/
-git commit -a --allow-empty-message -m ''
-git push -f heroku public:master
-git checkout master
